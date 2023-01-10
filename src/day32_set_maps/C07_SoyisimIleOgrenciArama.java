4466d0798ba47041d25f5b12234c3754221faaa6
murat sever <gencaga724@gmail.com>
2023-01-10 14:08:35 +0300

    32'nci Gun Sonu

--- src/day32_set_maps/C07_SoyisimIleOgrenciArama.java
+++ src/day32_set_maps/C07_SoyisimIleOgrenciArama.java
@@ -1,9 +1,17 @@
 package day32_set_maps;
 
+import java.util.Map;
+
 public class C07_SoyisimIleOgrenciArama {
     public static void main(String[] args) {
 
         // ogrenci map'te aradigimiz soyisimdeki ogrencilerin
-        // value'lerini yazdiran bir method olusturun
+        // isim,soyisim,sinif ve sube bilgilerini
+        // bir tablo olarak yazdiran bir method olusturun
+
+        Map<Integer,String>ogrenciMap =MapMethodDepo.ogrenciMapOlustur();
+
+        MapMethodDepo.soyisimIleOgrenciListesiYazdir(ogrenciMap,"Can");
+
     }
 }
